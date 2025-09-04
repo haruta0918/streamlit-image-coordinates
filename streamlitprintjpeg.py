e63363170b668282c5c2f745257fdb6c90173694
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-09-04 19:31:01 +0900

    Now

--- streamlitprintjpeg.py
+++ streamlitprintjpeg.py
@@ -10,33 +10,9 @@ import os
 import os
 folder_path = 'いじる用'
 t=0
-
-# フォルダ内の .jpg ファイルを探してループ
-for filename in os.listdir(folder_path):
-    if filename.endswith('.jpg'):
-        if t==0:
-            print("1回目")
-            # .jsonファイルを取得して並び替え（オプション）
-            jpg_files = [f for f in os.listdir(folder_path) if f.endswith('.jpg')]
-            jpg_files.sort()  # ソートすると見やすい順に並ぶ
-
-            folder_path2=folder_path+'/'    
-            a2=0
-            a=jpg_files[a2]
-            stra=str(a)
-            c=folder_path2+stra
-            d = [c,]
-        else:
-            a2+=1
-            a=jpg_files[a2]
-            stra=str(a)
-            c=folder_path2+stra
-            d.append(c)
-        t+=1
-print(d[1]) 
 e=0
-for filename in os.listdir(folder_path):
-    if filename.endswith('.jpg'):       
-        value = streamlit_image_coordinates(d[e])
-        e+=1
+value = streamlit_image_coordinates("いじる用/20250415_165815.jpg")
 
+            
+
+      
