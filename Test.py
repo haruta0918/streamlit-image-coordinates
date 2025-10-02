4016953c82369637fe5dc07d2fe92a3044a86961
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-10-02 21:13:05 +0900

    Test

--- Test.py
+++ Test.py
@@ -1,5 +1,38 @@
 import streamlit as st
+
+
+
+
+from streamlit_image_coordinates import streamlit_image_coordinates
+import streamlit as st
+
+import pandas as pd
+import numpy as np
+import json
+import os
+import streamlit as st
 from PIL import Image
+import os
+import streamlit as st
+from PIL import Image
+
+import glob
+import os
+import streamlit as st
+from PIL import Image
+folder_path = "実験中"
+
+# フォルダ内のjpgファイルをすべて取得
+jpg_files = glob.glob(os.path.join(folder_path, "*.jpg"))
+
+count=len(jpg_files)
+y=0
+z=jpg_files[y]
+
+for y in range(count):
+        img = Image.open(z)
+        st.image(img)
+        z=jpg_files[y]
 
 st.title("画像ファイルを表示")
 
