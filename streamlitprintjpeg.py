4608e650e3e0dc10ef1a64b4a6e2eafa0edece2b
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-10-02 20:54:57 +0900

    Now

--- streamlitprintjpeg.py
+++ streamlitprintjpeg.py
@@ -44,4 +44,13 @@ for y in range(count):
         st.image(img)
         z=jpg_files[y]
         st.write
+import streamlit as st
+from PIL import Image
 
+st.title("画像ファイルを表示")
+
+uploaded_file = st.file_uploader("画像を選んでください", type=["png", "jpg", "jpeg"])
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    st.image(img, caption="アップロードした画像")
