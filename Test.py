a0fc0e461b666775c5f2fa943661e3adc67ea23e
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-10-09 20:54:28 +0900

    test

--- Test.py
+++ Test.py
@@ -20,24 +20,32 @@ import glob
 import os
 import streamlit as st
 from PIL import Image
-folder_path = "実験中"
+import streamlit as st
+
+file_id = "1AbCdEfGhIjKlMnOp"  # あなたの画像IDに置き換えて
+image_url = f"https://drive.google.com/drive/folders/1Jb1y8mWmZ6MHm6omil7r4rEEbV4ERU7C?usp=drive_link={file_id}"
+
+st.image(image_url, caption="Google Driveから表示", use_column_width=True)
+
+# folder_path = "実験中"
+# 
 
 # フォルダ内のjpgファイルをすべて取得
-jpg_files = glob.glob(os.path.join(folder_path, "*.jpg"))
+# jpg_files = glob.glob(os.path.join(folder_path, "*.jpg"))
 
-count=len(jpg_files)
-y=0
-z=jpg_files[y]
+# count=len(jpg_files)
+# y=0
+# z=jpg_files[y]
 
-for y in range(count):
-        img = Image.open(z)
-        st.image(img)
-        z=jpg_files[y]
+# for y in range(count):
+#         img = Image.open(z)
+#         st.image(img)
+#         z=jpg_files[y]
 
-st.title("画像ファイルを表示")
+# st.title("画像ファイルを表示")
 
-uploaded_file = st.file_uploader("画像を選んでください", type=["png", "jpg", "jpeg"])
+# uploaded_file = st.file_uploader("画像を選んでください", type=["png", "jpg", "jpeg"])
 
-if uploaded_file is not None:
-    img = Image.open(uploaded_file)
-    st.image(img, caption="アップロードした画像")
+# if uploaded_file is not None:
+#     img = Image.open(uploaded_file)
+#     st.image(img, caption="アップロードした画像")
